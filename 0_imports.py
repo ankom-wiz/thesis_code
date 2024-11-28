ad045c8722ac418f1fed7edb6e66091b18ef24bf
Anastasios Komiotis <anastkomiotis@gmail.com>
2024-11-28 20:13:16 +0200

    Create 0_imports.py
    Import libraries

---
+++ 0_imports.py
@@ -0,0 +1,19 @@
+#general purpose libraires
+import math
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import fiona
+import cartopy
+import shapefile
+#visualization libraries
+%matplotlib inline
+import matplotlib.pyplot as plt
+#project modules
+import gnssr4river.fresnel.fresnelzone
+import gnssr4river.fresnel.plotfresnel
+import gnssr4river.fresnel.iterfresnel
+import gnssr4river.fresnel.getorbits
+import gnssr4river.fresnel.geod
+import gnssr4river.fresnel.intersect
+import gnssr4river.refl.nmea
