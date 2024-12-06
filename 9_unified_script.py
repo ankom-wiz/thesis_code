00069753c003b8f523bdb8782397210604f20320
Anastasios Komiotis <anastkomiotis@gmail.com>
2024-12-07 00:25:11 +0200

    Update 9_unified_script.py

--- 9_unified_script.py
+++ 9_unified_script.py
@@ -1,1 +1,38 @@
+#######################################################################
+#######################################################################
+# 0) initial imports
+#######################################################################
+#######################################################################
+
+#pip install git+https://github.com/ITC-Water-Resources/gnssr4water.git
+# ^ in case #project modules won't properly import
+
+#general purpose libraires
+import math
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import fiona
+import cartopy
+import shapefile
+#visualization libraries
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+# ^^^ SO FAR SO GOOD IT WORKS
+
+#project modules
+import gnssr4river.fresnel.fresnelzone
+import gnssr4river.fresnel.plotfresnel
+import gnssr4river.fresnel.iterfresnel
+import gnssr4river.fresnel.getorbits
+import gnssr4river.fresnel.geod
+import gnssr4river.fresnel.intersect
+import gnssr4river.refl.nmea
+
+#######################################################################
+#######################################################################
+# 1) nmea stream code
+#######################################################################
+#######################################################################
 
