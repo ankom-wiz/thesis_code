2f84f20619ea9d13c57f77fc6396ba2fa37dd722
Ankom <test@kjfg>
2025-03-07 14:19:53 +0200

    add script template

---
+++ xx_test.py
@@ -0,0 +1,18 @@
+#!/home/ankom/gimathesis/thesis_code/pygima/bin/python3
+
+#from gnssr4water.sites.skymask import SkyMask
+
+
+
+
+
+
+def main():
+    print("test")
+
+
+
+
+
+if  __name__ == "__main__":
+    main()
